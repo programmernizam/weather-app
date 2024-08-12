f939bcebb94b35a88feeb79bd8dac9fc2f459dbf
programmerNizam <mdnizam12109@gmail.com>
2024-08-12 19:55:21 +0600

    Project Components created with hard code

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import Weather from "./components/Weather/Weather";
 
 export default function App() {
   return (
-    <div>
+    <div className="bg-body font-[Roboto] text-light bg-[url('./src/assets/body-bg.png')] bg-no-repeat bg-cover h-screen grid place-items-center">
       <Header />
-      <Weather />
+      <main>
+        <section>
+          <Weather />
+        </section>
+      </main>
     </div>
   );
 }
